974a5b07c6f120f485ddcec90a2113fb775ae7c1
meenacodz <divyansh.meena9424@gmail.com>
2025-11-20 00:20:21 +0530

    Implement menubar button functionality
    Added functionality for menubar buttons to toggle display of corresponding sections.

--- script.js
+++ script.js
@@ -1,23 +1,107 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll("button").forEach((btn) => {
-    btn.addEventListener("click", function (e) {
-      const circle = document.createElement("span");
-      circle.classList.add("ripple");
 
-      const rect = btn.getBoundingClientRect();
-      const x = e.clientX - rect.left;
-      const y = e.clientY - rect.top;
+const menubarBtn1 = document.querySelectorAll(".menubar-btns")[0];
+const menubarBtn2 = document.querySelectorAll(".menubar-btns")[1];
+const menubarBtn3 = document.querySelectorAll(".menubar-btns")[2];
+const menubarBtn4 = document.querySelectorAll(".menubar-btns")[3];
 
-      circle.style.left = x + "px";
-      circle.style.top = y + "px";
-      circle.style.width = circle.style.height = Math.max(rect.width, rect.height) + "px";
+const divNotes = document.querySelector('.notes');
+const divPractice = document.querySelector('.practice');
+const divSupport = document.querySelector(".support");
+const divContact = document.querySelector(".contact")
 
-      btn.appendChild(circle);
+const igCard = document.getElementById("igCard");
+const ytCard = document.getElementById("ytCard");
 
-      setTimeout(() => {
-        circle.remove();
-      }, 600);
-    });
-  });
+let selected = 'notes';
+let notes = () => {
+  selected = 'notes';
+  menubarBtn1.style.borderBottom = '5px solid red';
+  menubarBtn1.style.color = 'red';
+
+  menubarBtn2.style.borderBottom = 'none';
+  menubarBtn2.style.color = 'white';
+  menubarBtn3.style.borderBottom = 'none';
+  menubarBtn3.style.color = 'white';
+  menubarBtn4.style.borderBottom = 'none';
+  menubarBtn4.style.color = 'white';
+
+  divNotes.style.display = "block";
+  divPractice.style.display = "none";
+  divSupport.style.display = "none";
+  divContact.style.display = "none";
+}
+let practice = () => {
+  selected = 'practice';
+  menubarBtn2.style.borderBottom = '5px solid red';
+  menubarBtn2.style.color = 'red';
+
+  menubarBtn1.style.borderBottom = 'none';
+  menubarBtn1.style.color = 'white';
+  menubarBtn3.style.borderBottom = 'none';
+  menubarBtn3.style.color = 'white';
+  menubarBtn4.style.borderBottom = 'none';
+  menubarBtn4.style.color = 'white';
+
+  divNotes.style.display = "none";
+  divPractice.style.display = "block";
+  divSupport.style.display = "none";
+  divContact.style.display = "none";
+}
+
+let support = () => {
+  selected = 'support';
+  menubarBtn3.style.borderBottom = '5px solid red';
+  menubarBtn3.style.color = 'red';
+
+  menubarBtn2.style.borderBottom = 'none';
+  menubarBtn2.style.color = 'white';
+  menubarBtn1.style.borderBottom = 'none';
+  menubarBtn1.style.color = 'white';
+  menubarBtn4.style.borderBottom = 'none';
+  menubarBtn4.style.color = 'white';
+
+  divNotes.style.display = "none";
+  divPractice.style.display = "none";
+  divSupport.style.display = "flex";
+  divContact.style.display = "none";
+}
+let contact = () => {
+  selected = 'contact';
+  menubarBtn4.style.borderBottom = '5px solid red';
+  menubarBtn4.style.color = 'red';
+
+  menubarBtn2.style.borderBottom = 'none';
+  menubarBtn2.style.color = 'white';
+  menubarBtn3.style.borderBottom = 'none';
+  menubarBtn3.style.color = 'white';
+  menubarBtn1.style.borderBottom = 'none';
+  menubarBtn1.style.color = 'white';
+
+  divNotes.style.display = "none";
+  divPractice.style.display = "none";
+  divSupport.style.display = "none";
+  divContact.style.display = "block";
+}
+
+menubarBtn1.addEventListener("click", notes);
+menubarBtn2.addEventListener("click", practice);
+menubarBtn3.addEventListener("click", support);
+menubarBtn4.addEventListener("click", contact);
+
+notes();
+
+
+
+
+
+
+
+ytCard.addEventListener("click", () => {
+  ytCard.classList.toggle("expanded");
 });
 
+
+
+igCard.addEventListener("click", () => {
+  igCard.classList.toggle("expanded");
+});
